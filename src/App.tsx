cb29f03ed67ccf4a2358439874291a857d944c81
Igor Brandalise <brandaliseigor123@gmail.com>
2025-06-19 00:18:31 -0300

    import

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./index.css";
 import { Header } from "./components/head";
+import { Projetos } from "./components/projetos";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
-    <>
+    <div className="min-h-[108vh]">
       <Header />
-    </>
+      <Projetos />
+      <hr className="mt-9 m-auto w-[350px] text-white/6 border-2" />
+      <Footer />
+    </div>
   );
 }
 
